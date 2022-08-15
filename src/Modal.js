1573a897e62f22bad140cd2cb772906f08c498c9
Sergio Valadez <sergioval@gmail.com>
2022-08-14 19:59:28 -0500

    :sparkles: Add modal

---
+++ src/Modal.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const MODAL_STYLES = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#FFF',
+  padding: '50px',
+  zIndex: 1000,
+  borderRadius: '5px'
+}
+
+const OVERLAY_STYLES = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, .7)',
+  zIndex: 1000
+}
+
+export default function Modal({ open, children, onClose }) {
+  if (!open) return null
+
+  return ReactDOM.createPortal (
+    <>
+      <div style={OVERLAY_STYLES} onClick={onClose} />
+      <div style={MODAL_STYLES}>
+        <button onClick={onClose}>close modal</button>
+        {children}
+      </div>
+    </>,
+    document.getElementById('portal')
+  )
+}
